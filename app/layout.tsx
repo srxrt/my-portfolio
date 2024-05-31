b2a075822fb927f46138d5efd3db325702d7616f
shokhrukh-rt <mr.shohruh.1003@gmail.com>
2024-05-31 17:13:18 +0900

    firstCommit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "Shokhrukh's Portfolio",
+	description: "Modern and minimalis portfolio",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
+}
