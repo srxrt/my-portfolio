9c1b240a094fc4cfadae073a5abc543e4efcafee
srxrt <str9703@gmail.com>
2024-06-04 01:21:23 +0900

    creating main section grid

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { gridItems } from "@/data";
 
 const Grid = () => {
-	return <div>Grid</div>;
+	return (
+		<section id="about">
+			<BentoGrid>
+				{gridItems.map(
+					({
+						id,
+						title,
+						description,
+						className,
+						img,
+						imgClassName,
+						titleClassName,
+						spareImg,
+					}) => (
+						<BentoGridItem
+							id={id}
+							key={id}
+							title={title}
+							className={className}
+							description={description}
+							img={img}
+							imgClassName={imgClassName}
+							titleClassName={titleClassName}
+							spareImg={spareImg}
+						/>
+					)
+				)}
+			</BentoGrid>
+		</section>
+	);
 };
 
 export default Grid;
