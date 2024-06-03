cd5c4f984fe459ad0f96ec53497b7e89e46c9df1
srxrt <str9703@gmail.com>
2024-06-03 23:36:13 +0900

    Finished the Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
 	return (
@@ -25,6 +28,22 @@ const Hero = () => {
 					<h2 className="uppercase tracking-widest text-xs text-center tex-blue-100 max-w-80">
 						Dynamic Web Magic with Next.js
 					</h2>
+					<TextGenerateEffect
+						className="text-center text-[40px] md:text-5xl lg:text-6xl"
+						words="Transforming Concepts into Seamless User Experiences"
+					/>
+
+					<p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+						{" "}
+						Hi, I&apos;m Shoxrux, a ReactJS developer based in South Korea
+					</p>
+					<a href="#about">
+						<MagicButton
+							title="Show my work"
+							icon={<FaLocationArrow />}
+							position="right"
+						/>
+					</a>
 				</div>
 			</div>
 		</div>
