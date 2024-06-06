c32a57d4869c9849b2ac7cdc8ac3b4cd60d08d69
srxrt <str9703@gmail.com>
2024-06-07 06:52:05 +0900

    creating the main section

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/utils/cn";
+import { BackgroundGradientAnimation } from "./GradientBg";
 
 export const BentoGrid = ({
 	className,
@@ -52,14 +53,50 @@ export const BentoGridItem = ({
 					"linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
 			}}
 		>
-			{}
-			<div className="group-hover/bento:translate-x-2 transition duration-200">
-				{}
-				<div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
-					{title}
+			<div className={`${id === 6 && "flex justify-center"} h-full`}>
+				<div className="w-full h-full absolute">
+					{img && (
+						<img
+							src={img}
+							alt={img}
+							className={cn(imgClassName, "object-cover, object-center")}
+						/>
+					)}
 				</div>
-				<div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-					{description}
+				<div
+					className={`absolute right-0 -bottom-5 ${
+						id === 5 && "w-full opacity-80"
+					}`}
+				>
+					{spareImg && (
+						<img
+							src={spareImg}
+							alt={spareImg}
+							className="object-cover object-center w-full h-full"
+						/>
+					)}
+				</div>
+				{id === 6 && (
+					<BackgroundGradientAnimation>
+						<div className="absolute z-50 flex items-center justify-center text-white font-bold" />
+					</BackgroundGradientAnimation>
+				)}
+
+				<div
+					className={cn(
+						titleClassName,
+						"group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+					)}
+				>
+					<div
+						className="font-sans font-extralight 
+					text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10"
+					>
+						{description}
+					</div>
+					<div className="font-sans font-bold text-lg text-neutral-600 lg:text-3xl max-w-96 z-10">
+						{title}
+					</div>
 				</div>
 			</div>
 		</div>
