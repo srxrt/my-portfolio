3d48125825576e35a8b971834153fcca5aa248bb
srxrt <str9703@gmail.com>
2024-06-07 23:55:35 +0900

    Third card done

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import { GlobeDemo } from "./GridGlobe";
 
 export const BentoGrid = ({
 	className,
@@ -97,6 +98,34 @@ export const BentoGridItem = ({
 					<div className="font-sans font-bold text-lg text-neutral-600 lg:text-3xl max-w-96 z-10">
 						{title}
 					</div>
+
+					{id === 2 && <GlobeDemo />}
+					{id === 3 && (
+						<div className="flex gap-1 lg:gap-4 w-fit absolute -right-3 lg:-right-2">
+							<div className="flex flex-col gap-3 lg:gap-8">
+								<span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+								{["React.js", "Next.js", "Typescript"].map((item) => (
+									<span
+										key={item}
+										className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132e]"
+									>
+										{item}
+									</span>
+								))}
+							</div>
+							<div className="flex flex-col gap-3 lg:gap-8">
+								{["Vue.js", "AWS.js", "MongoDB"].map((item) => (
+									<span
+										key={item}
+										className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132e]"
+									>
+										{item}
+									</span>
+								))}
+								<span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+							</div>
+						</div>
+					)}
 				</div>
 			</div>
 		</div>
