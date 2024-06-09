a7bd5808f29f60e78f31e50dc81910af31168b1c
srxrt <str9703@gmail.com>
2024-06-09 23:48:05 +0900

    finished projects section

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -2,6 +2,7 @@ import { projects } from "@/data";
 import React from "react";
 import { div } from "three/examples/jsm/nodes/Nodes.js";
 import { PinContainer } from "./ui/3d-pin";
+import { FaLocationArrow } from "react-icons/fa";
 
 const RecentProjects = () => {
 	return (
@@ -16,7 +17,7 @@ const RecentProjects = () => {
 						key={id}
 						className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
 					>
-						<PinContainer title={title} href={link}>
+						<PinContainer title={link} href={link}>
 							<div className="relative flex items-center justify-center sm:w-96 w-[80vh] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
 								<div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#12162d]">
 									<img src="/bg.png" alt="bg-img" />
@@ -29,6 +30,26 @@ const RecentProjects = () => {
 							<p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
 								{des}
 							</p>
+
+							<div className="flex items-center justify-between mt-7 mb-3">
+								<div className="flex items-center">
+									{iconLists.map((icon, index) => (
+										<div
+											key={icon}
+											className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+											style={{ transform: `translateX(-${5 * index * 2}px)` }}
+										>
+											<img src={icon} alt={icon} className="p-2 " />
+										</div>
+									))}
+								</div>
+								<div className="flex justify-center items-center">
+									<p className="flex lg:text-xl md:text-xs text-sm text-purple">
+										Check Live Site
+									</p>
+									<FaLocationArrow className="ms-3" color="#cbacf9" />
+								</div>
+							</div>
 						</PinContainer>
 					</div>
 				))}
