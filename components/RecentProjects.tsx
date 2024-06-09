042536e1c44bd92c404561f02a741dcaed1487ad
srxrt <str9703@gmail.com>
2024-06-09 23:32:23 +0900

    creating projects section

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,6 +1,7 @@
 import { projects } from "@/data";
 import React from "react";
 import { div } from "three/examples/jsm/nodes/Nodes.js";
+import { PinContainer } from "./ui/3d-pin";
 
 const RecentProjects = () => {
 	return (
@@ -15,7 +16,20 @@ const RecentProjects = () => {
 						key={id}
 						className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
 					>
-						{title}
+						<PinContainer title={title} href={link}>
+							<div className="relative flex items-center justify-center sm:w-96 w-[80vh] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+								<div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#12162d]">
+									<img src="/bg.png" alt="bg-img" />
+								</div>
+								<img src={img} alt={title} className="z-10 absolute bottom-0" />
+							</div>
+							<h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+								{title}
+							</h1>
+							<p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
+								{des}
+							</p>
+						</PinContainer>
 					</div>
 				))}
 			</div>
