c6b42479e52d7a3ca564765251104d209aba991b
srxrt <str9703@gmail.com>
2024-06-13 03:03:46 +0900

    updated and  finished testimonials section

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { InfiniteMovingCards } from "./ui/InfiniteMovingCards";
 import { companies, testimonials } from "@/data";
-import { div } from "three/examples/jsm/nodes/Nodes.js";
 
 const Clients = () => {
 	return (
-		<div className="py-20" id="projects">
+		<div className="py-20" id="testimonials">
 			<h1 className="heading">
 				Kind words from <span className="text-purple">satisfied clients</span>
 			</h1>
@@ -16,7 +15,7 @@ const Clients = () => {
 					speed="slow"
 				/>
 
-				<div>
+				<div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
 					{companies.map(({ id, img, name, nameImg }) => (
 						<div className="flex md:max-w-60 max-w-32 gap-2" key={id}>
 							<img src={img} alt={name} className="md:w-10 w-5" />
